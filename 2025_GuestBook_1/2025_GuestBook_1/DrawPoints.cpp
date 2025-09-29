d4a79961b2db02d675368bd1a7db93c6f2b5f17c
202407042 <minseo5830@yuhan.ac.kr>
2025-09-29 23:54:09 +0900

    병합

--- 2025_GuestBook_1/2025_GuestBook_1/DrawPoints.cpp
+++ 2025_GuestBook_1/2025_GuestBook_1/DrawPoints.cpp
@@ -1,18 +1,24 @@
+
 #include "DrawPoints.h"
+#include <chrono>
 #include <vector>
 
 /// COLORREF 적용 전
 /// storck 제거
 
-DrawPoints::DrawPoints()
+DrawPoints::DrawPoints() = default;
+
+void DrawPoints::saveToPoint(int x, int y,uint32_t drawTime)
 {
-	/// 그냥 만들어봣어요
-}
-void DrawPoints::saveToPoint(int x, int y)
-{
-	PenInit.push_back({ x, y});
+	PenInit.push_back({ x, y,drawTime});
 }
 
+/// 리플레이를 위한 getter 메서드 생성
+const std::vector<PenData>& DrawPoints::getPenInit()
+{
+    return PenInit;
+
+}
 void DrawPoints::drawToPoint(HDC hdc)
 {
     if (PenInit.size() < 2) return; // 점 2개 이상 있어야 선 그려짐
--- 2025_GuestBook_1/2025_GuestBook_1/DrawPoints.h
+++ 2025_GuestBook_1/2025_GuestBook_1/DrawPoints.h
@@ -1,20 +1,28 @@
 #pragma once
 #include "PenView.h"
+#include <chrono>
 #include <vector>
 
 struct PenData {
-	int x, y, strock;
+	int x, y;
+	uint32_t drawTime;
 	/// 색상을 이후 color와 연결 후 적용
-	// COLORREF penColor;
+	COLORREF penColor;
 };
+
 
 class DrawPoints
 {
 private:
-	std::vector<PenData> PenInit;
+
 public:
-	/// COLORREF 적용 전
+	/// 백터 PenInit public으로 이동 
+	std::vector<PenData> PenInit;
+
 	DrawPoints();
-	void saveToPoint(int x, int y);
+	/// COLORREF 적용 전
+	/// 백터에 데이터 저장 시 펜 굵기, 시간도 함께 저장
+	/// 시간은 조금 더 공부 후 chrono 활용해서 저장 예정.
+	void saveToPoint(int x, int y,uint32_t drawTime);
 	void drawToPoint(HDC hdc);
 };
